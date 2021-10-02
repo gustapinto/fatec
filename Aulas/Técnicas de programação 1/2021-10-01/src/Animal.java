9738391f85b3de28158b8bd546f52076b7b19521
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-10-02 08:14:57 -0300

    Java - 20211001

---
+++ Aulas/Técnicas de programação 1/2021-10-01/src/Animal.java
@@ -0,0 +1,3 @@
+interface Animal {
+    void mover(String locomocao);
+}
---
+++ Aulas/Técnicas de programação 1/2021-10-01/src/Coelho.java
@@ -0,0 +1,6 @@
+public class Coelho implements Animal {
+    @Override
+    public void mover(String locomocao) {
+        System.out.println("O coelho " + locomocao);
+    }
+}
