b02293884f00d8a795398c33b513dcd8f756b9c1
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-10-12 16:27:28 -0300

    Corrige require_once não funcionando em cadastraSistema.php

--- Aulas/Desenvolvimento web 2/2021-10-05/comandos/cadastraSistema.php
+++ Aulas/Desenvolvimento web 2/2021-10-05/comandos/cadastraSistema.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once('../src/Modelos/SistemasModelo.php');
-require_once('../src/Helpers/StringHelper.php');
-require_once('../src/Helpers/UrlHelper.php');
-require_once('../src/Redirecionador.php');
+require_once __DIR__ . '/../src/Modelos/SistemasModelo.php';
+require_once __DIR__ . '/../src/Helpers/StringHelper.php';
+require_once __DIR__ . '/../src/Helpers/UrlHelper.php';
+require_once __DIR__ . '/../src/Redirecionador.php';
 
 $textoAleatorio = StringHelper::geraStringAleatoria();
 $linkEncurtado = UrlHelper::geraUrl($textoAleatorio);
