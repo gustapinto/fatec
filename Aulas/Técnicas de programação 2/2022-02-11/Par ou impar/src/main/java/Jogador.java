d3e03255a1a57a58cf2ed90ef921c3f9e769c3ba
Gustavo <ghp2201@gmail.com>
2022-02-16 07:30:04 -0300

    Modifica par ou impar

--- Aulas/Técnicas de programação 2/2022-02-11/Par ou impar/src/main/java/Jogador.java
+++ Aulas/Técnicas de programação 2/2022-02-11/Par ou impar/src/main/java/Jogador.java
@@ -1,3 +1,5 @@
+package parimpar;
+
 import java.util.Scanner;
 
 public class Jogador {
@@ -5,12 +7,11 @@ public class Jogador {
     private int jogada;
     private String nome;
 
-
-    public void setEscolha(int escolha) {
+    public void setEscolha() {
         Scanner input = new Scanner(System.in);
 
         while (this.escolha != 0 && this.escolha != 1) {
-            System.out.println("Escolha");
+            System.out.println("Escolha: ");
             System.out.println("0 - Par");
             System.out.println("1 - Impar");
 
@@ -18,18 +19,42 @@ public class Jogador {
         }
 
         input.close();
+
+        this.mostraEscolha()
+    }
+
+    public void setEscolha(int escolha) {
+        this.escolha = (escolha == 1) ? 0 : 1
+
+        this.mostraEscolha()
     }
 
     public int getEscolha() {
         return this.escolha;
     }
 
-    public void setJogada(int jogada) {
-        this.jogada = jogada;
+    public void setJogada() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Jogada: ");
+
+        this.jogada = input.nextInt();
+
+        input.close();
     }
 
     public int getJogada() {
         return this.jogada;
+    }
+
+    public void setNome() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Nome: ");
+
+        this.nome = input.next();
+
+        input.close();
     }
 
     public void setNome(String nome) {
@@ -38,5 +63,11 @@ public class Jogador {
 
     public String getNome() {
         return this.nome;
+    }
+
+    public void mostraEscolha() {
+        String escolhaTexto = (this.escolha == 1) ? "Impar" : "Par";
+
+        System.out.println(this.nome + " escolheu: " + escolhaTexto)
     }
 }
--- Aulas/Técnicas de programação 2/2022-02-11/Par ou impar/src/main/java/Main.java
+++ Aulas/Técnicas de programação 2/2022-02-11/Par ou impar/src/main/java/Main.java
@@ -1,5 +1,14 @@
+package parimpar;
+
 public class Main {
     public static void main(String []args) {
-        //
+        Jogador usuario = new Jogador();
+        Jogador maquina = new Jogador();
+
+        usuario.setNome();
+        maquina.setNome("Computador");
+
+        usuario.setEscolha();
+        maquina.setEscolha(usuario.getEscolha());
     }
 }
