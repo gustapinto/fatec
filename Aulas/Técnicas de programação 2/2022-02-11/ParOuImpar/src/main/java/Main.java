929938541b4caebfe01a28e4313097c08d66a1b5
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-02-18 19:38:21 -0300

    Add randomização de escolha

--- Aulas/Técnicas de programação 2/2022-02-11/ParOuImpar/src/main/java/Main.java
+++ Aulas/Técnicas de programação 2/2022-02-11/ParOuImpar/src/main/java/Main.java
@@ -4,13 +4,22 @@ public class Main {
     public static void main(String []args) {
         Jogador usuario = new Jogador();
         Jogador maquina = new Jogador();
-        Random rand = new Random();
+        Random rand = new Random(System.currentTimeMillis());
 
         usuario.setNome();
         maquina.setNome("Computador");
 
-        usuario.setEscolha();
-        maquina.setEscolha(usuario.getEscolha());
+        if (rand.nextInt(10) % 2 == 0) {
+            System.out.println("O computador escolhe");
+
+            maquina.setEscolha(rand.nextInt(2));
+            usuario.setEscolha(maquina.getEscolha());
+        } else {
+            System.out.println("O jogador escolhe");
+
+            usuario.setEscolha();
+            maquina.setEscolha(usuario.getEscolha());
+        }
 
         usuario.setJogada();
         maquina.setJogada(rand.nextInt(10));
