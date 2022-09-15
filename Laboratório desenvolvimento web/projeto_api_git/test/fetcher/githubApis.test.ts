66b0d3c9e743cd090c50565c618ab909f9681d49
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-09-15 19:15:31 -0300

    Corrige import

--- Laboratório desenvolvimento web/projeto_api_git/test/fetcher/githubApis.test.ts
+++ Laboratório desenvolvimento web/projeto_api_git/test/fetcher/githubApis.test.ts
@@ -1,4 +1,4 @@
-import { GithubAPIRepositoryFetcher } from '@sr'
+import { GithubAPIRepositoryFetcher } from '@src/fetcher/githubApi'
 
 describe('Unit testing for the Github API Fetcher', (): void => {
     it('Test getAllRepositories must not return a empty Array', async (): Promise<void> => {
